37166f3717c53f3181a0e20ec84ea7281f6862bb
sarath246 <sarathps2129@gmail.com>
2025-09-02 11:24:07 +0530

    Commit - Changed class to interface

--- inventory-restock-api/src/main/java/com/inventory/restock/InventoryRestockApplication.java
+++ inventory-restock-api/src/main/java/com/inventory/restock/InventoryRestockApplication.java
@@ -3,7 +3,7 @@ package com.inventory.restock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.inventory.*")
 public class InventoryRestockApplication {
 
 	public static void main(String[] args) {
--- inventory-restock-api/src/main/java/com/inventory/valueobject/InventoryRestockProductsRequest.java
+++ inventory-restock-api/src/main/java/com/inventory/valueobject/InventoryRestockProductsRequest.java
@@ -1,9 +1,7 @@
 package com.inventory.valueobject;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 import java.util.List;
 
 public record InventoryRestockProductsRequest(
